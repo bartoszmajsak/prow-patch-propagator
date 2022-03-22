440b05581031899215b86d0ae86c154839035823
bartoszmajsak <bartosz.majsak@gmail.com>
2022-03-22 14:44:32 +0100

    feat(script): adds ability to replace multiple placeholders at once

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -4,14 +4,14 @@ set -euo pipefail
 
 function placeholders() {
   local path=$1
-  local old=$2
-  local new=$3
   local cmd=( "sed" )
 
-  if [ -n "${4+x}" ]; then
-      cmd+=( -i )
-  fi
-  cmd+=( -e "s@${old}@${new}@g" "${path}")
+  for ((i=2; i<=$#; i=i+2))
+  do
+    local j=$((i+1))
+    cmd+=( -e "s@${!i}@${!j}@g" )
+  done
+  cmd+=("${path}")
 
   "${cmd[@]}"
 }
